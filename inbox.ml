96b4f797cad520a13aed122a3ee836543d79e3d1
Tikhon Jelvis <tikhon@jelv.is>
2014-10-08 18:32:45 -0700

    Continued working on stubs for the API.

--- inbox.ml
+++ inbox.ml
@@ -1,2 +1,4 @@
 
-let get_namespaces = failwith "undefined"
+let get_namespaces () = failwith "undefined"
+
+let get_namespace id = failwith "undefined"
--- inbox.mli
+++ inbox.mli
@@ -3,3 +3,8 @@ open Core
 (** Returns a list of all the namespaces defined in the Inbox app. *)
 val get_namespaces : unit -> namespace list Lwt.t
 
+(** Return the namespace with the given id or none if it doesn't
+ *  exist.
+ *)
+val get_namespace : namespace_id -> namespace option Lwt.t
+
