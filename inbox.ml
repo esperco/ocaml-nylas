ec0d238e60c75b8244a07ddfe5af6c346dd0f11b
Tikhon Jelvis <tikhon@jelv.is>
2014-10-14 10:44:16 -0700

    Updated types so that code actually builds...

--- inbox.ml
+++ inbox.ml
@@ -5,14 +5,14 @@ open Cohttp_lwt_unix
 exception Error_code of Cohttp.Code.status_code
 
 type app = {
-  api_uri    : string;
-  base_uri   : string;
+  api_uri    : Uri.t;
+  base_uri   : Uri.t;
   app_id     : string;
   app_secret : string;
 }
 
 let authentication_uri app user_email redirect_uri =
-  Uri.add_query_param app.base_uri [
+  Uri.add_query_params' app.base_uri [
     ("client_id", app.app_id);
     ("response_type", "code");
     ("scope", "email");
@@ -21,7 +21,7 @@ let authentication_uri app user_email redirect_uri =
   ]
 
 let post_authentication_code app code =
-  let uri = Uri.add_query_param app.base_uri [
+  let uri = Uri.add_query_params' app.base_uri [
       ("client_id", app.app_id);
       ("client_secret", app.app_secret);
       ("grant_type", "authorization_code");
@@ -30,7 +30,7 @@ let post_authentication_code app code =
   in
   Client.post uri
 
-let request_string http_method ?access_token uri =
+let call_string http_method ?access_token uri =
   let uri = match access_token with
     | Some token -> Uri.add_query_param' uri ("access_token", token)
     | None       -> uri
@@ -41,6 +41,6 @@ let request_string http_method ?access_token uri =
   | `OK, `Empty       -> return ""
   | err, _            -> raise (Error_code err)
 
-let get_namespaces () = get_string (Uri.of_string "https://api.inbox.com/n")
+let get_namespaces () = call_string `GET (Uri.of_string "https://api.inbox.com/n")
 
 let get_namespace id = failwith "undefined"
