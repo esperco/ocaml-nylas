5deb43c5ca2820644dd207ce431355edbd2d59c6
Tikhon Jelvis <tikhon@jelv.is>
2014-10-15 20:49:48 +0000

    Extended the server with some authentication stuff.

--- example/example.ml
+++ example/example.ml
@@ -10,11 +10,17 @@ open Cohttp_lwt_unix.Server
 
 open Printf
 
+open App_config
+
 let callback conn { Request.uri } body =
   printf "Serving a request!\n";
   printf "Uri: %s\n" (Uri.to_string uri);
   flush stdout;
-  respond_string ~status:`OK ~body:(Uri.to_string uri) ()
+  match Uri.get_query_param uri "code" with
+  | None      -> respond_string ~status:`OK ~body:("Not authorized!\n No code returned.") ()
+  | Some code ->
+     Inbox.post_authentication_code app code >>= fun token -> 
+     respond_string ~status:`OK ~body:(sprintf "Authorized! Response: %s." token) ()
 
 let conn_closed _ () = printf "Connection closed\n!"
          
@@ -22,4 +28,4 @@ let config = { callback; conn_closed };;
 
 printf "Creating server!\n";
 flush stdout;
-Lwt_unix.run (Server.create ~mode:`TCP config ~address:"localhost" ~port:8888)
+Lwt_unix.run (Server.create ~mode:`TCP config ~address:"10.136.63.208" ~port:8042)
