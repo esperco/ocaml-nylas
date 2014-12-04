b85536928c9a335acd1b874fd53b658d1dffe1eb
Jeff Meister <nanaki@gmail.com>
2014-12-03 17:11:37 -0800

    Event create/update, delta sync

--- types/time.ml
+++ types/time.ml
@@ -13,9 +13,13 @@ let clean_some_time some_time = match some_time.st_obj with
     | Some ts_start_time, Some ts_end_time ->
        `Timespan { ts_obj = Some `Timespan; ts_start_time; ts_end_time }
     | _, _ -> raise (Malformed_time `Timespan))
-  | Some `Date -> (match some_time.st_start_date with
-    | Some d_start_date -> `Date { d_obj = Some `Date; d_start_date }
-    | None              -> raise (Malformed_time `Date))
+  | Some `Date ->
+      (match some_time.st_date with
+      | Some d_start_date -> `Date { d_obj = Some `Date; d_start_date }
+      | None ->
+          (match some_time.st_start_date with
+          | Some d_start_date -> `Date { d_obj = Some `Date; d_start_date }
+          | None              -> raise (Malformed_time `Date)))
   | Some `Datespan -> (match some_time.st_start_date, some_time.st_end_date with
     | Some ds_start_date, Some ds_end_date ->
        `Datespan { ds_obj = Some `Datespan; ds_start_date; ds_end_date }
